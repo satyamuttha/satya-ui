174c92fc4e725c3f9c7a96dda10a482b159f8b6e
satyamuttha <79783794+satyamuttha@users.noreply.github.com>
2024-09-24 21:26:29 +0530

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("multiplication table\n");
+    for(i=1;i<=10;i++)
+    {
+        printf("%d X %d = %d\n",5,i,5*i);
+    }
+    return 0;
+}
